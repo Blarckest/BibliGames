e89f1aabf4e835d2e5af1484186c5e88c5616e46
yogourves <yogourves@9f2383a8-1bda-4610-8ce3-eb415e5371c0>
2021-04-15 21:13:14 +0000

    Debut du modele: -ajout de la fonctionnalité de recherche automatique des dossiers de jeux (Steam/Uplay/Epic/Riot) -ajout de la fonctionnalité de recherche automatique des executable de jeux(Steam/Uplay/Epic/Riot<-probleme pour lui voir commentaire ligne 40) -ajout d'une classe de test (juste pour lancer le programme sans la vue) -ajout de l'enum Launcher -ajout de la classe Jeu qui contiendra toutes les infos utiles (pas complété il y juste les propriété pour l'instant)

---
+++ Modele/Jeu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modele
+{
+    public class Jeu
+    {
+        string Dossier;
+        string Executable;
+        string Detail;
+        string Note;
+        string Nom;
+        string Image;
+    }
+}
