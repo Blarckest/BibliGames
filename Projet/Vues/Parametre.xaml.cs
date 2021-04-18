c816f16a55ad9cc842c81b3657ee48fa857eeeef
yogourves <yogourves@9f2383a8-1bda-4610-8ce3-eb415e5371c0>
2021-04-18 15:21:09 +0000

    Integration du FolderExplorer dans la fenetre parametre

--- Projet/Vues/Parametre.xaml.cs
+++ Projet/Vues/Parametre.xaml.cs
@@ -26,22 +26,29 @@ namespace Vues
         }
         public void ParcourirDossiers(object sender, MouseButtonEventArgs e)
         {
-            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
-            dlg.InitialDirectory = "C:"; // chemin de depart
-            dlg.Title = "Selectionner un dossier"; // nom fenetre
-            dlg.Filter = "Directory|*.a.directory"; // ne montre pas les fichiers seulement les dossier (en donnant comme filtre l'extension:".a.directory"
-            dlg.FileName = "select"; // nom par defaut: "select.a.directory" avec l'extension .a.directory -> chemin\\select.a.directory.a.directory
+            //Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            //dlg.InitialDirectory = "C:"; // chemin de depart
+            //dlg.Title = "Selectionner un dossier"; // nom fenetre
+            //dlg.Filter = "Directory|*.a.directory"; // ne montre pas les fichiers seulement les dossier (en donnant comme filtre l'extension:".a.directory"
+            //dlg.FileName = "select"; // nom par defaut: "select.a.directory" avec l'extension .a.directory -> chemin\\select.a.directory.a.directory
 
-            if (dlg.ShowDialog() == true)
+            //if (dlg.ShowDialog() == true)
+            //{
+            //    string path = dlg.FileName;
+            //    path = path.Replace("\\select.a.directory", ""); //enleve le nom du fichier du chemin -> chemin.a.directory
+            //    path = path.Replace(".a.directory", ""); //enleve l'extension -> chemin
+            //    if (!System.IO.Directory.Exists(path)) //si l'utilisateur a changer le nom de base on créé le dossier
+            //    {
+            //        System.IO.Directory.CreateDirectory(path);
+            //    }
+            //    ListeFolder.Items.Add(path);
+            //}
+            FolderExplorer FolderExplorer = new FolderExplorer();
+            FolderExplorer.ShowDialog();
+            string Dossier = FolderExplorer.DossierSelectionner;
+            if (Dossier!=null)
             {
-                string path = dlg.FileName;
-                path = path.Replace("\\select.a.directory", ""); //enleve le nom du fichier du chemin -> chemin.a.directory
-                path = path.Replace(".a.directory", ""); //enleve l'extension -> chemin
-                if (!System.IO.Directory.Exists(path)) //si l'utilisateur a changer le nom de base on créé le dossier
-                {
-                    System.IO.Directory.CreateDirectory(path);
-                }
-                ListeFolder.Items.Add(path);
+                ListeFolder.Items.Add(Dossier);
             }
         }
         private void Annuler(object sender, RoutedEventArgs e)
