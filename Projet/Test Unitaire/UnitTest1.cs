39f28d8873460f3db3a3dca2760e83af43d2aae2
yogourves <yogourves@9f2383a8-1bda-4610-8ce3-eb415e5371c0>
2021-05-06 09:33:32 +0000

    test unitaire dependance fonctionne

---
+++ Projet/Test Unitaire/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Modele;
+using System;
+using Xunit;
+
+namespace TestUnitaire
+{
+    public class UnitTest1
+    {
+        
+        [Fact]
+        public void Test1()
+        {
+           
+
+        }
+    }
+}
