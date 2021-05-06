4f9b3d28205ed89a27d334fd3e55b7010d69b2b1
trchallet <trchallet@9f2383a8-1bda-4610-8ce3-eb415e5371c0>
2021-05-06 10:03:19 +0000

    ajout de tests Unitaires

--- Projet/Test Unitaire/UnitTest1.cs
+++ Projet/Test Unitaire/UnitTest1.cs
@@ -8,10 +8,20 @@ namespace TestUnitaire
     {
         
         [Fact]
-        public void Test1()
+        public void TestJeu()
         {
-           
+            Jeu jeu = new Jeu("Valorant", "", "Valorant.exe", LauncherName.Riot);
 
+            jeu.Description=$"{jeu.Nom} est un jeux de tir FPS.";
+            jeu.Image = $"{jeu.Nom}.png";
+            jeu.Dossier = @"/programme/Riot";
+            jeu.Nom = "RocketLeague";
+            jeu.Exec = "RocketLeague.exe";
+            jeu.Launcher = LauncherName.EpicGames;
+            jeu.Icone = "voiture.png"; 
+            jeu.Note = "monter Gold";
+
+            Jeu jeu2 = new Jeu("GTA5", "", "", "GTA5.png", "Trevor.png", "finir les braquages", "GTA5 est un jeu open world");
         }
     }
 }
