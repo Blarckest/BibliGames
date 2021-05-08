70122910cfacd47e0c36a1452eb26fb985b443f3
yogourves <yogourves@9f2383a8-1bda-4610-8ce3-eb415e5371c0>
2021-05-08 21:46:17 +0000

    Diagramme mis a jour(normalement tout y est) suppression d'un fonction qui servait a rien

--- Projet/Vues/User Controls/LauncherUC.xaml.cs
+++ Projet/Vues/User Controls/LauncherUC.xaml.cs
@@ -22,24 +22,5 @@ namespace Vues.User_Controls
         {
             InitializeComponent();
         }
-        private void UCMouseWheelEvent(object sender, MouseWheelEventArgs e) //permet au user control d'interagir avec la list view quand l'utilisateur scroll
-        {
-            ListViewItem Item;
-            StackPanel send = sender as StackPanel;
-            DependencyObject Temp = send;
-            while (!(Temp is ListViewItem)) //tant que l'on trouve pas notre listViewItem dans la hierarchie on remonte
-            {
-                Temp = VisualTreeHelper.GetParent(Temp);
-            }
-            Item = Temp as ListViewItem;
-            if (Item != null)
-            {
-                //creation de l'event a envoyer
-                MouseWheelEventArgs args = new MouseWheelEventArgs(e.MouseDevice, e.Timestamp, e.Delta);
-                args.RoutedEvent = ListViewItem.MouseWheelEvent;
-                args.Source = sender;
-                Item.RaiseEvent(args); //envoi!
-            }
-        }
     }
 }
