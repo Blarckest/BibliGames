c3754ff143c63ea4d158e2ba1a765fc894a094d8
trchallet <trchallet@9f2383a8-1bda-4610-8ce3-eb415e5371c0>
2021-05-18 15:47:13 +0000

    début de modification de Logs pour introduire un thread

--- Projet/Logger/Logs.cs
+++ Projet/Logger/Logs.cs
@@ -1,37 +1,43 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.IO;
+using System.Threading;
 
 namespace Logger
 {
     public static class Logs
     {
+        private static Thread Thread = new Thread(SaveLog);
         private static string Dossier = "./Logs";
-        private static DateTime Date = DateTime.Now;
+        private static ConcurrentQueue<string> Queue= new ConcurrentQueue<string>();
 
-        private static void SaveLog(string Action, string Type)
+        private static void SaveLog()
         {
             return;
             Directory.CreateDirectory(Dossier);
             StreamWriter Sw = new StreamWriter($"{Dossier}/FichierDeLogs.txt", true);
-            Sw.WriteLine(Date);
-            Sw.WriteLine($"{Type} : {Action}");
-            Sw.WriteLine();
+            Sw.WriteLine($"{Date} | {Type} : {Action}\n");
             Sw.Close();
         }
 
         public static void InfoLog(string Action)
         {
-            SaveLog(Action, "Info");
+            Queue.Enqueue($"{DateTime.Now} | Info : {Action}");
+            if (!Thread.IsAlive)
+            {
+
+            }
+            SaveLog(Queue.TryDequeue());
         }
 
         public static void ErrorLog(string Action)
         {
-            SaveLog(Action, "Error");
+            Queue.Enqueue($"{DateTime.Now} | Error : {Action}");
         }
 
         public static void WarningLog(string Action)
         {
-            SaveLog(Action, "Warning");
+            Queue.Enqueue($"{DateTime.Now} | Warning : {Action}");
         }
 
         public static void SuppLog()
