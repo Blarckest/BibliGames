3dc58a5e9ca2e72a6acb2eb3ab83a584dab2da0c
yogourves <yogourves@9f2383a8-1bda-4610-8ce3-eb415e5371c0>
2021-05-20 16:02:12 +0000

    Push des modif de ce matin

---
+++ Projet/Vues/User Controls/DetailsJeu.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Modele;
+
+namespace Vues.User_Controls
+{
+    /// <summary>
+    /// Logique d'interaction pour Details.xaml
+    /// </summary>
+    public partial class Details : UserControl
+    {
+        public Details()
+        {
+            InitializeComponent();
+        }
+
+        private void ModifierJeu(object sender, RoutedEventArgs e)
+        {
+            AjoutDetailWindow window = new AjoutDetailWindow(DataContext as Jeu);
+            window.ShowDialog();
+        }
+
+        private void LancerJeu(object sender, RoutedEventArgs e)
+        {
+            //lancer le jeu
+            bool trouver = false;
+            var temp = VisualTreeHelper.GetParent(this);
+            while (!trouver) //tant qu'on trouve pas un manager on remonte
+            {
+                if (temp.GetType() == typeof(windowParts.MasterDetail))
+                {
+                    (temp.GetValue(DataContextProperty) as Manager).LancerJeu();
+                    trouver = true;
+                }
+                temp = VisualTreeHelper.GetParent(temp);
+            }
+        }
+    }
+}
---
+++ Projet/Vues/User Controls/JeuUC.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Modele;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vues.User_Controls
+{
+    /// <summary>
+    /// Logique d'interaction pour JeuUC.xaml
+    /// </summary>
+    public partial class JeuUC : UserControl
+    {
+        public JeuUC()
+        {
+            InitializeComponent();
+        }
+
+        private void DemandeSuppression(object sender, MouseButtonEventArgs e)
+        {
+            bool trouver = false;
+            var temp = VisualTreeHelper.GetParent(this);
+            while (!trouver) //on set le selecteditem a l'item courant
+            {
+                if (temp.GetType() == typeof(ListBox))
+                {
+                    (temp as ListBox).SelectedItem = DataContext;
+                    trouver = true;
+                }
+                temp = VisualTreeHelper.GetParent(temp);
+            }
+
+            if (MessageBox.Show("Supprimer ce jeu?", "Suppression", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+            {
+                trouver = false;
+                temp = VisualTreeHelper.GetParent(this);
+                while (!trouver) //tant qu'on trouve pas un manager on remonte
+                {
+                    if (temp.GetType() == typeof(ListBox))
+                    {
+                        (temp.GetValue(DataContextProperty) as Manager).SuppJeu();
+                        trouver = true;
+                    }
+                    temp = VisualTreeHelper.GetParent(temp);
+                }
+            }
+        }
+
+        private void MontrerCroix(object sender, MouseEventArgs e)
+        {
+            CroixSupp.Visibility = Visibility.Visible;
+        }
+
+        private void CacherCroix(object sender, MouseEventArgs e)
+        {
+            CroixSupp.Visibility = Visibility.Hidden;
+        }
+    }
+}
