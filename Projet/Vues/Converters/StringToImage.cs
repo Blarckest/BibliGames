c7c8689406aade4cb110df210e2b7a77435a2d44
yogourves <yogourves@9f2383a8-1bda-4610-8ce3-eb415e5371c0>
2021-05-27 12:18:47 +0000

    push du Converters (oubli du commit dernier)

---
+++ Projet/Vues/Converters/StringToImage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Windows.Data;
+
+namespace Vues.Converters
+{
+    class StringToImage : IValueConverter
+    {
+        private string pathToFolderExec = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);//pour avoir le chemin jusqu'a l'executable
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string imagePath = value as string;
+            if (!string.IsNullOrWhiteSpace(imagePath))
+            {
+                return new Uri(Path.Combine(pathToFolderExec, imagePath), UriKind.RelativeOrAbsolute);
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
