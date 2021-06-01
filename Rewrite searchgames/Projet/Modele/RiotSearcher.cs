eda50ae8c8702f5370220bc403e1bf6d435169ab
trchallet <trchallet@9f2383a8-1bda-4610-8ce3-eb415e5371c0>
2021-06-01 09:56:27 +0000

    ajout des classes RiotSearcher et SteamSearcher

--- Rewrite searchgames/Projet/Modele/RiotSearcher.cs
+++ Rewrite searchgames/Projet/Modele/RiotSearcher.cs
@@ -1,19 +1,51 @@
-﻿using System;
+﻿using Logger;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Modele
 {
     public class RiotSearcher : GameSearcher
     {
+        private const string regKey = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall";
+        Dictionary<string, string> Direct = new Dictionary<string, string>();
         protected override void GetGames()
         {
-            throw new NotImplementedException();
+            if (Dossiers != null)
+            {
+                foreach (var dossier in Direct)
+                {
+                    string[] nomExecutables = Directory.GetFiles(dossier.Value, "*.exe", SearchOption.AllDirectories);
+                    var executable = Filter(nomExecutables, dossier.Key, LauncherName.Riot);
+                    Jeux.Add(new Jeu(dossier.Key, dossier.Value, executable, LauncherName.Riot));
+                    Logs.InfoLog($"Ajout du jeu {dossier.Key}");
+                }
+                Jeux.Sort();
+            }
+            else
+            {
+                GetGamesDirectory();
+                GetGames();
+            }
         }
 
         protected override void GetGamesDirectory()
         {
-            throw new NotImplementedException();
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(regKey);
+            foreach (string subKey in key.GetSubKeyNames()) //parcour des sous-clé
+            {
+                if (subKey.Contains("Riot Game")) //cas ou la sous-clé nous interesse
+                {
+                    RegistryKey keyJeu = Registry.CurrentUser.OpenSubKey(regKey + subKey);
+                    string path = keyJeu.GetValue("InstallLocation").ToString();
+                    path = path.Replace("/", "\\"); //certains jeux sont marque avec des / et d'autres avec des \\ donc on transforme ceux en / en \\
+                    string nom = keyJeu.GetValue("DisplayName").ToString();
+                    Dossiers.Add(path);
+                    Direct.Add(nom, path);
+                }
+            }
         }
     }
 }
--- Rewrite searchgames/Projet/Modele/SteamSearcher.cs
+++ Rewrite searchgames/Projet/Modele/SteamSearcher.cs
@@ -1,19 +1,109 @@
-﻿using System;
+﻿using Logger;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Modele
 {
     public class SteamSearcher : GameSearcher
     {
+        private const string steam = "SOFTWARE\\Wow6432Node\\Valve\\Steam\\";
         protected override void GetGames()
         {
-            throw new NotImplementedException();
+            List<Jeu> temp = new List<Jeu>();
+            string nom = "";
+            string folderName = "";
+            List<string> steamAppsVisited = new List<string>();
+            foreach (string dossier in Dossiers) //sert juste a parcourir les differents steamapps
+            {
+                string pathToSteamApps = Directory.GetParent(Directory.GetParent(dossier).FullName).FullName;
+                if (!steamAppsVisited.Contains(pathToSteamApps)) //si on pas deja traiter ce steamapps
+                {
+                    string[] allFiles = Directory.GetFiles(pathToSteamApps, "*.acf"); //ce dossier contient tout les fichiers de config de tout les jeux
+                    foreach (string file in allFiles)
+                    {
+                        if (System.IO.File.Exists(file))
+                        {
+                            string[] lines = System.IO.File.ReadAllLines(file);
+                            foreach (string line in lines) //parcour du fichier
+                            {
+                                if (line.Contains("name")) //recuperation du nom
+                                {
+                                    nom = line.Substring(10);
+                                    nom = nom.Replace("\"", "");
+                                }
+                                else if (line.Contains("installdir")) //recuperation du dossier
+                                {
+                                    folderName = line.Substring(16);
+                                    folderName = folderName.Replace("\"", "");
+                                    folderName = $"{pathToSteamApps}{@"\common\"}{folderName}";
+                                    break;
+                                }
+                            }
+                            if (nom != "Steamworks Common Redistributables") //Ce dossier n'est pas un jeu
+                            {
+                                Jeu jeu = SearchForExecutables(folderName, LauncherName.Steam);
+                                jeu.Nom = nom;
+                                temp.Add(jeu);
+                                Logs.InfoLog($"Ajout du jeu {nom}");
+
+                            }
+                        }
+                    }
+                    steamAppsVisited.Add(pathToSteamApps);
+                }
+            }
+            temp.Sort();
+            Jeux.AddRange(temp);
         }
 
         protected override void GetGamesDirectory()
         {
-            throw new NotImplementedException();
+            List<string> paths = new List<string>();
+
+            RegistryKey key;
+            if ((key = Registry.LocalMachine.OpenSubKey(steam)) != null)
+            {
+                string steamPath = key.GetValue("InstallPath").ToString();
+                string configPath = steamPath + "/steamapps/libraryfolders.vdf";
+                string regexChemin = @"[A-Z]:\\"; //cherche pour un debut de chemin ex: D:\\
+                if (File.Exists(configPath))
+                {
+                    string[] configLines = File.ReadAllLines(configPath);
+                    foreach (string line in configLines)
+                    {
+                        Match res = Regex.Match(line, regexChemin);
+                        if (line != string.Empty && res.Success)
+                        {
+                            string matched = res.ToString();  //recupere la partie qui a trigger la regex ex D:\\
+                            string path = line.Substring(line.IndexOf(matched)); //prend a partir de D:\\ jusqua la fin de la ligne
+                            path = path.Replace("\\\\", "\\");  //tout les  \ sont echapé on a donc besoin d'en enlever 
+                            path = path.Replace("\"", "\\");  //met les dernier \ à \\
+                            if (Directory.Exists(path + "steamapps\\common"))
+                            {
+                                path += "steamapps\\common\\";
+                                paths.Add(path);
+                            }
+                        }
+                    }
+                    paths.Add(steamPath + "\\steamapps\\common\\");
+                }
+
+                foreach (string path in paths)
+                {
+                    string[] allDir = Directory.GetDirectories(path);
+                    foreach (string directory in allDir)
+                    {
+                        if (!(IsDirectoryEmpty(directory) || directory.Contains("Steamworks Shared"))) //on veux pas de certains dossier
+                        {
+                            Dossiers.Add(directory);
+                        }
+                    }
+                }
+            }
         }
     }
 }
